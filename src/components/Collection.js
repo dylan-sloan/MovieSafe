4bb1efa5c8020cb4ba0ceb2de39ad0f6dc739e95
Dylan Sloan <dylansloanage@gmail.com>
2022-10-24 21:50:13 -0400

    collection pg update

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useNavigate } from "react-router-dom";
+import './styling.css'
 
 const Collection = () => {
   const navigate = useNavigate();
@@ -7,8 +8,15 @@ const Collection = () => {
   return (
     <div>
         <h1 className="heading"> Your Collection </h1>
+        <div className='movieList'>
+          <li>Keep track of the movies you've watched!</li>
+          <li>They will show up here after you've watched them.</li>
+          <li>Grow your collection!</li>
+          <li>COMING SOON</li>
+        </div>
         <br />
-        <button onClick={() => navigate(-1)}>Back to Your List</button>
+        <br />
+        <button className='movieList' onClick={() => navigate(-1)}>Back to Your List</button>
     </div>
   )
 }
