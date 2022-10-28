6b01bc00b94ae92191cf7d7318de91fe40b92bce
Dylan Sloan <dylansloanage@gmail.com>
2022-10-28 14:52:59 -0400

    added empty message

--- src/components/WatchList.js
+++ src/components/WatchList.js
@@ -1,7 +1,19 @@
 import React from 'react'
 import Movie from './Movie'
+import './styling.css'
 
 export default function WatchList({ movies, toggleWatched }) {
+  //If no movies are in the list then display the message
+  if(Object.keys(movies).length === 0) {
+    return (
+      <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: 30 }}>
+        Add a movie to start your list
+        <br />
+        <br />
+      </div>
+    )
+  }
+
   return (
     movies.map(movie => {
         return <Movie key={movie.id} toggleWatched={toggleWatched} movie={movie} />
