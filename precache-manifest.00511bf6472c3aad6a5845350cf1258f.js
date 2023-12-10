e36d20f2037d0b0b19d99824c25f5402021eb3e5
Dylan Sloan <dylansloanage@gmail.com>
2023-12-10 14:23:48 -0500

    Updates

--- precache-manifest.00511bf6472c3aad6a5845350cf1258f.js
+++ precache-manifest.00511bf6472c3aad6a5845350cf1258f.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "f96d87cc8cb5535b2ba8",
-    "url": "/MovieSafe/static/css/main.bcfc6af1.chunk.css"
+    "revision": "75b2b544596a2ee2435e",
+    "url": "/MovieSafe/static/css/main.865a2b0b.chunk.css"
   },
   {
-    "revision": "f96d87cc8cb5535b2ba8",
-    "url": "/MovieSafe/static/js/main.f96d87cc.chunk.js"
+    "revision": "75b2b544596a2ee2435e",
+    "url": "/MovieSafe/static/js/main.75b2b544.chunk.js"
   },
   {
     "revision": "9019674f54aa45f4ec17",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/MovieSafe/static/js/runtime~main.eceb91c9.js"
   },
   {
-    "revision": "1a2ec56c2605132a6c1decb3a0229141",
+    "revision": "7d9d6df2f2478492ccfb36eafe263f1e",
     "url": "/MovieSafe/index.html"
   }
 ];
